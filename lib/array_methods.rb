a124494ff8846851cddab448010aa19e50f8e15b
Mark Enright <enright.mark@gmail.com>
2019-08-07 20:54:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   answer = 0
   counter = 0
   while counter < array.length do 
-    if array[counter] == value_to_find{
+    if array[counter] == value_to_find 
       answer = counter
       counter += 1
     end
